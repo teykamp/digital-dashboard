6a8c9020b72495cb098fc15e487612fdebba4e23
eykamt <embergameslp@gmail.com>
2024-04-11 22:35:42 -0400

    add dseg font

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <RevTest />
+  
 </template>
 
 <script setup lang="ts">
+// https://github.com/keshikan/DSEG
+import 'dseg/css/dseg.css' // globally
 import RevTest from './components/RevTest.vue'
 </script>
