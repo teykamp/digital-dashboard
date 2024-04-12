0fd537187183aba302ff6cfc03c55743bf675dc9
shotgunfacelift <shotgunfacelift762@protonmail.com>
2024-04-12 19:03:52 -0400

    added gauges and formatted

--- src/components/S2000.vue
+++ src/components/S2000.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
+    <img src="../assets/s2k.svg" alt=""
+    height="400rem">
+    
+  </div>
+
+  <div>
     <div
       :style="{
         // transform: 'skew(-10deg)',
         background: '#110000',
-        height: `${DIGIT_HEIGHT}px`,
-        width: `${TOTAL_WIDTH}px`,
-        padding: '20px',
+        // height: `${DIGIT_HEIGHT}px`,
+        // width: `${TOTAL_WIDTH}px`,
+        height: `90px`,
+        width: `260px`,
+        padding: '15px',
+        position: 'absolute',
+        top: '250px',
+        left: '528px',
+        zIndex: '0'
       }"
     >
       <div 
@@ -15,7 +27,7 @@
             'padding-left': `${(index - 1) * (DIDGIT_WIDTH + SEGMENT_SPACING)}px`,
           }"
       >
-        <SegmentDisplay 
+        <SegmentDisplay
           :display-number="computeDigits(speed, true)[index - 1]"
           :car-type="'S2000'"
           :color="'#FC1E3E'"
@@ -47,3 +59,10 @@ const DIDGIT_WIDTH = SEGMENT_WIDTH * 2 + SEGMENT_HEIGHT + 1
 const TOTAL_WIDTH = DIDGIT_WIDTH * 3 + 1 + SEGMENT_SPACING * 2
 
 </script>
+
+<style>
+img{
+  position: relative;
+  z-index: 2;
+}
+</style>
