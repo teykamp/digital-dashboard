176feeddc8ec64dd53c189b7f805b557b8ed2b78
eykamt <embergameslp@gmail.com>
2024-04-11 22:46:41 -0400

    extract engine logic into composable

--- src/components/RevTest.vue
+++ src/components/RevTest.vue
@@ -33,85 +33,12 @@
 
 <script setup lang="ts">
 import SegmentDisplay from './SegmentDisplay.vue'
-import computeDigits from '../composables/useComputeDigits'
+import computeDigits from '../functions/useComputeDigits'
+import useEngineController from '../composables/useEngineController'
 
-import { ref, onMounted, onUnmounted } from 'vue'
+const { speed } = useEngineController()
 
-// CONSTANTS  
-const MAX_GEARS = 6
-const MAX_RPM = 8000
-const MIN_RPM = 800
-const GEAR_RATIOS = [0.1, 0.15, 0.3, 0.4, 0.6, 0.8]
 const SEGMENT_HEIGHT = 35
 const SEGMENT_WIDTH = 10
-//
 
-const speed = ref(0)
-const rpm = ref(0)
-const rpmGain = ref(0)
-const isAccelerating = ref(false)
-const isShifting = ref(false)
-const gear = ref(1)
-
-const shiftGear = () => {
-  isShifting.value = false
-}
-
-
-const handleKeyPress = (event: KeyboardEvent) => {
-  if (event.code === "Space") return isAccelerating.value = true
-  if (event.code === "ArrowUp") {
-    isShifting.value = true
-    setTimeout(shiftGear, 150)
-    gear.value = Math.min(gear.value + 1, MAX_GEARS)
-    rpmGain.value -= Math.sqrt(rpm.value)
-    return
-  }
-  if (event.code === "ArrowDown") {
-    gear.value = Math.max(gear.value - 1, 1)
-    isShifting.value = true
-    setTimeout(shiftGear, 150)
-    rpm.value += Math.sqrt(rpm.value) * 40
-    return
-  }
-}
-
-const handleKeyUp = (event: KeyboardEvent) => {
-  if (event.code === "Space") isAccelerating.value = false
-}
-
-const gameLoopActive = ref(false)
-
-const gameLoop = () => {
-  if (isAccelerating.value && !isShifting.value) {
-    rpm.value += 85
-    speed.value += 2 * GEAR_RATIOS[gear.value - 1]
-  }    
-
-  if (rpm.value > MAX_RPM) rpm.value *= 0.95
-  rpm.value = Math.max(rpm.value * .99, MIN_RPM)
-  
-  rpmGain.value *= (0.99)
-
-  rpm.value += rpmGain.value
-
-  speed.value *= (0.99)
-
-  if (gameLoopActive.value) {
-    requestAnimationFrame(gameLoop)
-  }
-}
-
-onMounted(() => {
-  window.addEventListener("keydown", handleKeyPress)
-  window.addEventListener("keyup", handleKeyUp)
-  gameLoopActive.value = true
-  gameLoop()
-})
-
-onUnmounted(() => {
-  window.removeEventListener("keydown", handleKeyPress)
-  window.removeEventListener("keyup", handleKeyUp)
-  gameLoopActive.value = false
-})
 </script>
