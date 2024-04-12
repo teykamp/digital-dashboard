90c9b387241b2da9704dec590e09ff683a84ede7
eykamt <embergameslp@gmail.com>
2024-04-11 23:31:48 -0400

    rename to S2K

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <RevTest />
+  <S2000 />
   
 </template>
 
 <script setup lang="ts">
 // https://github.com/keshikan/DSEG
 import 'dseg/css/dseg.css' // globally
-import RevTest from './components/RevTest.vue'
+import S2000 from './components/S2000.vue'
 </script>
---
+++ src/components/S2000.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    :style="{
+      background: '#110000',
+      height: `${TOTAL_HEIGHT}px`,
+      width: `${(TOTAL_WIDTH) * 3 + SPACING_BETWEEN_SEGMENTS - 9}px`,
+    }"
+  >
+    <div 
+      v-for="index in 3"
+      :style="{
+          // transform: 'skew(-10deg)',
+          'padding-left': `${(index - 1) * SPACING_BETWEEN_SEGMENTS}px`,
+        }"
+    >
+      <SegmentDisplay 
+        :display-number="computeDigits(speed)[index - 1]"
+        :car-type="'S2000'"
+        :color="'#FC1E3E'"
+        :dimensions="{
+          width: SEGMENT_WIDTH,
+          height: SEGMENT_HEIGHT
+        }"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SegmentDisplay from './SegmentDisplay.vue'
+import computeDigits from '../functions/useComputeDigits'
+import useEngineController from '../composables/useEngineController'
+
+const { speed } = useEngineController()
+
+const SEGMENT_HEIGHT = 35
+const SEGMENT_WIDTH = 10
+const SPACING_BETWEEN_SEGMENTS = SEGMENT_HEIGHT + 2 * SEGMENT_WIDTH + 20
+
+const TOTAL_HEIGHT = SEGMENT_HEIGHT * 2 + SEGMENT_WIDTH + 4
+const TOTAL_WIDTH = SEGMENT_WIDTH + SEGMENT_HEIGHT + 2
+
+</script>
