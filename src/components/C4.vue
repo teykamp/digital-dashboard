231b7f794d8f7b7c652d0393f17e2a35b39284cf
eykamt <embergameslp@gmail.com>
2024-05-01 00:41:28 -0400

    add tachometer

--- src/components/C4.vue
+++ src/components/C4.vue
@@ -20,9 +20,24 @@
         position: 'absolute',
         height: `${SEGMENT_HEIGHT}px`,
         width: '270px',
-        background: 'darkgreen',
+        background: '#639D59',
         bottom: `${(SEGMENT_HEIGHT + 1) * index}px`,
-        zIndex: '0'
+        boxShadow: '0 0 5px #639D59',
+      }"
+    ></div>
+  </div>
+
+  <div style="position: relative; margin-top: -60px; left: 500px; top: -100px">
+    <div 
+      v-for="index in computeRpmIndexes"
+      :style="{
+        position: 'absolute',
+        height: '270px',
+        width: `${SEGMENT_HEIGHT}px`,
+        left: `${(SEGMENT_HEIGHT + 1) * index + 330}px`,
+        bottom: '6px',
+        background: index >= (TACHOMETER_SEGMENTS - 7) ? (index >= (TACHOMETER_SEGMENTS - 3) ? '#DA4D3F' : '#E6A325') : '#639D59',
+        boxShadow: index >= (TACHOMETER_SEGMENTS - 7) ? (index >= (TACHOMETER_SEGMENTS - 3) ? '0 0 5px #DA4D3F' : '0 0 5px #E6A325') : '0 0 5px #639D59',
       }"
     ></div>
   </div>
@@ -41,11 +56,12 @@ const { speed, rpm, MAX_RPM } = useEngineController()
 
 
 const SPEEDOMETER_SEGMENTS = 34
-const TACHOMETER_SEGMENTS = 26
+const TACHOMETER_SEGMENTS = 30
 const SEGMENT_HEIGHT = 7
 const MAX_SPEED = 85
 
 const computeSpeedIndexes = computed(() => Math.round(Math.min(speed.value / MAX_SPEED, 1) * SPEEDOMETER_SEGMENTS))
+const computeRpmIndexes = computed(() => Math.round(rpm.value / MAX_RPM * TACHOMETER_SEGMENTS))
 
 const { trip, toggleTrip, speedometerMode, toggleSpeedometerMode, signals } = useDashButtons([
   {
