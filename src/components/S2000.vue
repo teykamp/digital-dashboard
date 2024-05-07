337053757134f511c36c146e38d4badb555407bd
eykamt <embergameslp@gmail.com>
2024-05-06 20:32:09 -0400

    change turn signals to for loop

--- src/components/S2000.vue
+++ src/components/S2000.vue
@@ -110,18 +110,13 @@
   <div
     style="position: relative; display: flex; justify-content: space-between; width: 500px; margin-top: -400px; z-index: 99; left: 370px; scale: 0.7;"
   >
-    <TurnSignal 
-      :style="{
-        opacity: signals[0] ? 1 : 0
-      }"
-      direction="left" 
-    />
-    <TurnSignal 
-      :style="{
-        opacity: signals[1] ? 1 : 0
-      }"
-      direction="right" 
-    />
+  <TurnSignal
+    v-for="index in 2" 
+    :style="{
+      opacity: signals[index - 1] ? 1 : 0
+    }"
+    :direction="index === 1 ? 'left' : 'right'" 
+  />
   </div>
 
 </template>
