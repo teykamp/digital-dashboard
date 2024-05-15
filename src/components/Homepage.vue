f48456176f150bc71ac52c1b6155994b97644006
shotgunfacelift <shotgunfacelift762@protonmail.com>
2024-05-14 22:51:39 -0400

    made rudimentary homepage

---
+++ src/components/Homepage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div style="position: absolute;">
+    <img style="position:relative; left: -1100px; top: -250px;" src="../assets/3d/anims/s2korange.gif" alt="">
+    </div>
+
+    <div style="position: absolute;">
+    <img style="position:relative; left: -460px; top: -250px;" src="../assets/3d/anims/c4orange.gif" alt="">
+    </div>
+
+    <div style="position: absolute;">
+    <img style="position:relative; left: 200px; top: -250px;" src="../assets/3d/anims/300zxorange.gif" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
